851563b7f462370d49efab1d9c2573ae6a4097d3
Camilo Alarcon <camilo4750@gmail.com>
2025-03-21 16:14:39 -0500

    fix(exception): structure

--- app/Exceptions/BaseException.php
+++ app/Exceptions/BaseException.php
@@ -1,21 +1,22 @@
 <?php
 
 namespace App\Exceptions;
+
 use App\Exceptions\Throwable;
 
 class BaseException extends \Exception
 {
-    protected $message = "";
-    protected $code= 0;
+    protected $message = '';
+    protected $code = 0;
     protected array $errors;
 
-    public function __construct(array $errors = [], string $message = "", int $code = 0, ?\Throwable $previous = null)
+    public function __construct(array $errors = [], string $message = null, int $code = 0, ?\Throwable $previous = null)
     {
         $this->errors = $errors;
         parent::__construct($message ?? $this->message, $code, $previous);
     }
 
-    public function getErrors():array
+    public function getErrors(): array
     {
         return $this->errors;
     }
