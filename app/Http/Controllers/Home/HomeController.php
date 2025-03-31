8668f61db6d732fed4d53994c5e3acbfa69ceb6c
Camilo Alarcon <camilo4750@gmail.com>
2025-03-31 12:11:49 -0500

    fix(test): Fix the feature tests, leaving them all functional

--- app/Http/Controllers/Home/HomeController.php
+++ app/Http/Controllers/Home/HomeController.php
@@ -3,10 +3,11 @@
 namespace App\Http\Controllers\Home;
 
 use App\Http\Controllers\Controller;
+use Illuminate\View\View;
 
 class HomeController extends Controller
 {
-    public function index($clientId)
+    public function index($clientId): View
     {
         return view('home', ['clientId' => $clientId]);
     }
